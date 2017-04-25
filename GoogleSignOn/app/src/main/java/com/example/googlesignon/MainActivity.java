9ef47c93698ad10e7900bc173fdcad7170fae9db
mores <stephen.more@gmail.com>
2017-04-25 08:24:56 -0700

    Adds button with Butterknife.

--- GoogleSignOn/app/src/main/java/com/example/googlesignon/MainActivity.java
+++ GoogleSignOn/app/src/main/java/com/example/googlesignon/MainActivity.java
@@ -5,9 +5,27 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "GoogleSignOn::MainActivity";
+
+    @butterknife.BindView( R.id.button1 )
+    protected android.widget.Button button1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+	android.util.Log.i( TAG, "onCreate" );
+	butterknife.ButterKnife.setDebug(true);
+	butterknife.ButterKnife.bind( this );
+
+	button1.setOnClickListener( new android.view.View.OnClickListener()
+	{
+		@Override
+		public void onClick( android.view.View v )
+		{
+			android.util.Log.i( TAG, "I have been pushed !" );
+		}
+	} );
     }
 }
